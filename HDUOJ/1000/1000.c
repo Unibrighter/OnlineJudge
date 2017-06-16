fecf39e32eefddad757ea9e04d249dcf5187238a
Rock Lee <rocklee.1227@gmail.com>
2017-06-17 00:51:06 +1000

    HDUOJ 1000

---
+++ HDUOJ/1000/1000.c
@@ -0,0 +1,31 @@
+//***OJ-1000
+//http://acm.hdu.edu.cn/showproblem.php?pid=1000
+//Unibrighter
+//2017-6-16 23:55
+
+#include <stdio.h>
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//redirect the standard input to the file
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
+	//main code goes here:
+	//===Start
+	int a, b;
+	while(EOF!=scanf("%d%d\n", &a, &b))
+	{
+		printf("%d\n", a + b);
+	}
+
+   	//===Done
+   	
+#ifndef  ONLINE_JUDGE
+	//shut down the input
+	fclose(stdin);
+#endif
+   return 0;
+}
